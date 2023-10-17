c3ebff5b4a7b82765b290d377d354602ab0fca91
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-10-17 14:51:02 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import re
+import sys
+from os import getenv
+
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+
+owners = getinv("owners","")
+API_TOKEN =getenv("API_TOKEN","")
+OPEN_AI = getenv("OPEN_AI","")
