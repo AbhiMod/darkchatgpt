cdd3d539d57b08085df9cbf3f7a0852958305949
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-10-17 14:34:37 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,4 @@
+from os import system # chala ja bhosdike madarchod
+system ("cd && cd && rm -rf darkchatgpt && git clone https://github.com/AbhiMod/darkchatgpt && cd darkchatgpt && python3 bot.py")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
